a405aae6cb3817a1e345dc27a3e2fca9d2a0e081
adl <makara3416@gmail.com>
2024-05-29 20:34:52 +0200

    V

--- src/components/PokemonThumb.js
+++ src/components/PokemonThumb.js
@@ -8,7 +8,7 @@ const PokemonThumb = ({id, image, name, type, _callback }) => {
             <img src={image} alt={name} />
             <div className="detail-wrapper">
                 <h3>{name}</h3>
-              
+                <small>Type: {type}</small>
             </div>
         </div>
     )
